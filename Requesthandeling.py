9acf384cc7520b2e62c3ead38eb2e41ff962db76
Pralad pandit <praladpandit2004@gmail.com>
2026-06-09 20:58:21 +0545

    Delete Requesthandeling.py

--- Requesthandeling.py
+++
@@ -1,9 +0,0 @@
-from flask import request
-from app import app
-@app.route('/user', methods=['POST'])
-def create_user():
-    data = request.get_json()
-    return{
-        "message": "user created",
-        "data": "data"
-    }
